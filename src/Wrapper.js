2919b2494b3c047a80febf7b19b97a790151543a
DanStockham <ds321609@gmail.com>
2017-12-24 23:36:36 -0500

    moved footer content to own component

--- src/Wrapper.js
+++ src/Wrapper.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import Nav from './Nav.js';
+import Footer from './components/Footer';
 
 export default class Wrapper extends Component {
     constructor() {
@@ -47,22 +48,12 @@ export default class Wrapper extends Component {
     render() {
       const styles = this.checkScrollState();
       return (
-        <div className="app">
-          <Nav passedStyles={styles}/>
-          {this.props.children}
-          <footer className="app-footer">
-            <ul className="app-footer__social-media">
-              <a className="social-media__link" rel="noopener noreferrer" href="https://github.com/danstockham" target="_blank"><li ><i className="fa fa-github-square" aria-hidden="true"></i></li></a>
-              <a className="social-media__link" rel="noopener noreferrer" href="https://www.linkedin.com/in/danstockham/" target="_blank"><li ><i className="fa fa-linkedin-square" aria-hidden="true"></i></li></a>
-              <a className="social-media__link" rel="noopener noreferrer" href="https://twitter.com/CodeMeAwesome" target="_blank"><li ><i className="fa fa-twitter-square" aria-hidden="true"></i></li></a>
-              <a className="social-media__link" rel="noopener noreferrer" href="https://www.freecodecamp.com/danstockham" target="_blank"><li ><i className="fa fa-free-code-camp" aria-hidden="true"></i></li></a>
-            </ul>
-            <span className="app-footer__resume">
-              <a rel="noopener noreferrer" href="http://goo.gl/A7Cbrt" target="_blank"><i className="fa fa-file-text-o"aria-hidden="true"></i>
-              <span>Checkout my resume here</span>
-              </a>
-            </span>
-          </footer>
+        <div>
+          <div className="app">
+            <Nav passedStyles={styles}/>
+            {this.props.children}
+            <Footer />
+          </div>
         </div>
       )
   }
---
+++ src/components/Footer.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Footer = (props) => {
+  return (
+    <footer className="app-footer">
+      <ul className="app-footer__social-media">
+        <a className="social-media__link" rel="noopener noreferrer" href="https://github.com/danstockham" target="_blank"><li ><i className="fa fa-github-square" aria-hidden="true"></i></li></a>
+        <a className="social-media__link" rel="noopener noreferrer" href="https://www.linkedin.com/in/danstockham/" target="_blank"><li ><i className="fa fa-linkedin-square" aria-hidden="true"></i></li></a>
+        <a className="social-media__link" rel="noopener noreferrer" href="https://twitter.com/CodeMeAwesome" target="_blank"><li ><i className="fa fa-twitter-square" aria-hidden="true"></i></li></a>
+        <a className="social-media__link" rel="noopener noreferrer" href="https://www.freecodecamp.com/danstockham" target="_blank"><li ><i className="fa fa-free-code-camp" aria-hidden="true"></i></li></a>
+      </ul>
+      <span className="app-footer__resume">
+        <a rel="noopener noreferrer" href="http://goo.gl/A7Cbrt" target="_blank"><i className="fa fa-file-text-o"aria-hidden="true"></i>
+        <span>Checkout my resume here</span>
+        </a>
+      </span>
+    </footer>
+  )
+}
+
+export default Footer
